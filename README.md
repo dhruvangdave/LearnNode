00f127a639c483ee7d640be7c56d10d4e407d3dd
dhruvangdave <dhruvang.dave@ghanshyamdigital.com>
2023-11-20 16:24:54 +0530

    Till Video 80

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ To start the project
 ```
 npm start
 ```
+
+###Templating Engines
+e.g. 
+EJS :- `<p><%= name %></p>`,
+Pug (Jade) :- `p # { name }`,
+Handlebars :- `<p>{{ name }}</p>`
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,10 @@ const express = require('express');
 const rootDir = require('../helpers/path')
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    console.log('In another middleware?');
+    console.log(adminData.products);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     // res.send('<h1>Hello from Express!</h1>'); // Printed
 });
